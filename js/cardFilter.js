d323643be162e2d36d22df8c0632d98b860e759a
Klaudia Król <100020626+KlaudiaK0205@users.noreply.github.com>
2024-10-06 15:31:28 +0200

    Update
    - header
    - hero
    - whatIDo
    - howOrder
    - myProjects

---
+++ js/cardFilter.js
@@ -0,0 +1,16 @@
+const filterButtons = document.querySelectorAll('.filter-btn');
+const productCards = document.querySelectorAll('.product-card');
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const category = button.getAttribute('data-category');
+
+        productCards.forEach(card => {
+            if (category === 'all' || card.getAttribute('data-category') === category) {
+                card.style.display = 'block'; // Pokaż produkt
+            } else {
+                card.style.display = 'none'; // Ukryj produkt
+            }
+        });
+    });
+});
